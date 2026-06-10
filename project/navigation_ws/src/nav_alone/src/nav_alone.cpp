5d1a08b2c58e277531f384d4b46ece8d12b6778d
Pedro H Aranguren <pedrohdezaranguren@gmail.com>
2026-06-10 23:09:27 +0200

    Correcting and improving navigation

--- project/navigation_ws/src/nav_alone/src/nav_alone.cpp
+++ project/navigation_ws/src/nav_alone/src/nav_alone.cpp
@@ -1,10 +1,11 @@
 /*****************************************************************************/
-//  First attemp of navigation alone
+//  Navigation with Laser Scan, Bumpers, IR Proximity & Adaptive Velocity
 /*****************************************************************************/
 
 #include <chrono>
 #include <memory>
 #include <cmath>
+#include <algorithm>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
@@ -17,6 +18,7 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/LinearMath/Matrix3x3.h"
 #include "irobot_create_msgs/msg/hazard_detection_vector.hpp"
+#include "irobot_create_msgs/msg/ir_intensity_vector.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -24,7 +26,6 @@ using std::placeholders::_1;
 class NavigationNode : public rclcpp::Node
 {
 public:
-   // Alias
    using Undock = irobot_create_msgs::action::Undock;
    using GoalHandleUndock = rclcpp_action::ClientGoalHandle<Undock>;
 
@@ -33,10 +34,11 @@ public:
      current_state_(RobotState::CHECKING_DOCK),
      object_detected_(false),
      target_angle_(0.0),
-     facing_direction_(0.0)
+     facing_direction_(0.0),
+     free_space_ahead_(1.0)
    {
       // Distance to stop
-      this->declare_parameter<float>("stop_distance", 1.0);
+      this->declare_parameter<float>("stop_distance", 0.35);
       stop_distance_ = this->get_parameter("stop_distance").as_double();
 
       // QoS Best Effort
@@ -73,6 +75,12 @@ public:
          "/hazard_detection", 
          qos_profile,
          std::bind(&NavigationNode::hazard_callback, this, _1));
+
+      // IR
+      ir_subscriber_ = this->create_subscription<irobot_create_msgs::msg::IrIntensityVector>(
+         "/ir_intensity", 
+         qos_profile,
+         std::bind(&NavigationNode::ir_callback, this, _1));
 
       // Timer
       timer_ = this->create_wall_timer(
@@ -150,42 +158,43 @@ private:
 
    void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
    {    
-      // Detect objects
-      bool local_object_detected = false;
-
+      float min_frontal_dist = msg->range_max;
+      // Check laser
       for (size_t i = 0; i < msg->ranges.size(); i++) 
       {
          float range = msg->ranges[i];
 
-         if (range < msg->range_min) 
-         {
-            local_object_detected = true;
-            break;
-         }
-         
          // Sensor can return infinite
          if (!std::isfinite(range) || range > msg->range_max)
          {
             continue;
          }
-         
+
+         /*if (range < msg->range_min) 
+         {
+            local_object_detected = true;
+            break;
+         }
+         */
+         // Calculate angle
          double angle = msg->angle_min + (i * msg->angle_increment);
          angle = atan2(sin(angle), cos(angle));
 
          if (std::abs(angle) <= (20.0 * M_PI / 180.0)) 
          {
+            if (range < min_frontal_dist) 
+            {
+               min_frontal_dist = range;
+            }
             if (range < stop_distance_) 
             {
-               local_object_detected = true;
+               object_detected_ = true;
                break; 
             }
          }
       }
-      
-      if (local_object_detected) 
-      {
-         object_detected_ = true;
-      }
+
+      free_space_ahead_ = min_frontal_dist;
 
       // Calculate new route
       if (object_detected_ && current_state_ == RobotState::WALKING)
@@ -196,24 +205,21 @@ private:
 
       if (current_state_ == RobotState::SCANNING)
       {
-         const int rays_per_window = 15; // This number should be variable, now scaning for 2 meters
          const int total_rays = msg->ranges.size();
-         const int num_windows = total_rays / rays_per_window;
+         const int window_size = 40;
          
-         float max_free_space = -1.0;
-         int best_window_index = -1;
+         float best_avg_dist = -1.0;
          float best_window_angle = 0.0;
 
-         for (int i = 0; i < num_windows; ++i) 
+         for (int i = 0; i < total_rays - window_size; i+=5) 
          {
-            float current_window_min = std::numeric_limits<float>::infinity();
+            float sum = 0.0;
+            int valid_rays = 0;
 
-            for (int j = 0; j < rays_per_window; ++j) 
+            for (int j = 0; j < window_size; ++j) 
             {
                // Obtain ray data
-               int ray_index = (i * rays_per_window) + j;
-               if (ray_index >= total_rays) break; 
-               float ray_value = msg->ranges[ray_index];
+               float ray_value = msg->ranges[i + j];
 
                // Check if ray data is valid
                if (!std::isfinite(ray_value) || ray_value > msg->range_max) 
@@ -225,37 +231,37 @@ private:
                   ray_value = 0.0;
                }
 
-               // Update window min
-               if (ray_value < current_window_min) 
+               // Update window value
+               sum += ray_value;
+               valid_rays++;
+            }
+
+            if (valid_rays > 0) 
+            {
+               float current_avg = sum / valid_rays;
+
+               int center_ray_index = i + (window_size / 2);
+               double window_angle = msg->angle_min + (center_ray_index * msg->angle_increment);
+               window_angle = atan2(sin(window_angle), cos(window_angle));
+
+               // Penalyze wall just deteted
+               if (object_detected_ && std::abs(window_angle) < (60.0 * M_PI / 180.0)) 
                {
-                  current_window_min = ray_value;
+                  current_avg *= 0.1; 
                }
-            }
 
-            int central_ray_index = (i * rays_per_window) + (rays_per_window / 2);
-            double window_angle = msg->angle_min + (central_ray_index * msg->angle_increment);
-
-            // Normalize
-            window_angle = atan2(sin(window_angle), cos(window_angle));
-
-            if (object_detected_ && std::abs(window_angle) < (45.0 * M_PI / 180.0)) 
-            {
-               current_window_min = -1.0;
-            }
-
-            // Update best window
-            if (current_window_min > max_free_space) 
-            {
-               max_free_space = current_window_min;
-               best_window_index = i;
-               best_window_angle = window_angle;
+               // Update best window
+               if (current_avg > best_avg_dist) 
+               {
+                  best_avg_dist = current_avg;
+                  best_window_angle = window_angle;
+               }
             }
          }
 
-         RCLCPP_INFO(this->get_logger(), "Window %d with direction %.2f with %.2f m free", 
-                        best_window_index, 
+         RCLCPP_INFO(this->get_logger(), "Going direction %.2f with %.2f m free (avg)", 
                         best_window_angle, 
-                        max_free_space);
+                        best_avg_dist);
                         
          double aux_angle = facing_direction_ + best_window_angle;
          target_angle_ = atan2(sin(aux_angle), cos(aux_angle));
@@ -296,7 +302,7 @@ private:
                      this->get_logger(), 
                      *this->get_clock(), 
                      1000, 
-                     "¡BUMPER ACTIVADO! Impacto detectado frente al robot."
+                     "Bumper Activated"
                   );
                   
                   object_detected_ = true;
@@ -306,6 +312,32 @@ private:
                      current_state_ = RobotState::SCANNING;
                   }
                }
+         }
+      }
+   }
+
+   void ir_callback(const irobot_create_msgs::msg::IrIntensityVector::SharedPtr msg)
+   {
+      // Minimal distance
+      const int ir_obstacle_threshold = 200; 
+
+      for (const auto& reading : msg->readings) 
+      {
+         if (reading.value > ir_obstacle_threshold) 
+         {
+            RCLCPP_WARN_THROTTLE(
+               this->get_logger(), 
+               *this->get_clock(), 
+               1000, 
+               "IR activated, object detected by sensor '%s' with intensity %d", 
+               reading.header.frame_id.c_str(), reading.value
+            );
+            
+            object_detected_ = true;
+            if (current_state_ == RobotState::WALKING) 
+            {
+               current_state_ = RobotState::SCANNING;
+            }
          }
       }
    }
@@ -340,7 +372,18 @@ private:
          }
          else
          {
-            msg.twist.linear.x = 0.3; 
+            if (current_state_ == RobotState::ROTATING)
+            {
+               object_detected_ = false;
+            }
+            
+            float max_speed = 0.45;
+            float min_speed = 0.08;
+            
+            // Calculate speed
+            float calculated_speed = free_space_ahead_ * 0.25; 
+
+            msg.twist.linear.x = std::max(min_speed, std::min(max_speed, calculated_speed));
             msg.twist.angular.z = 0.0;
             current_state_ = RobotState::WALKING;
          }
@@ -353,12 +396,15 @@ private:
    float stop_distance_;
    float target_angle_;
    double facing_direction_;
+   float free_space_ahead_;
+   
    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr movement_publisher_;
    rclcpp::Subscription<irobot_create_msgs::msg::DockStatus>::SharedPtr dock_subscriber_;
    rclcpp_action::Client<Undock>::SharedPtr undock_client_;
    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_subscriber_;
    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber_;
    rclcpp::Subscription<irobot_create_msgs::msg::HazardDetectionVector>::SharedPtr hazard_subscriber_;
+   rclcpp::Subscription<irobot_create_msgs::msg::IrIntensityVector>::SharedPtr ir_subscriber_;
    rclcpp::TimerBase::SharedPtr timer_;
 };
 
